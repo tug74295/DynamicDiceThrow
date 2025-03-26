3fdbd166d282ac5b1075f135544a73486f1d4bca
Kostandin Jorgji <tug74295@temple.edu>
2025-03-26 15:24:28 -0400

    Add fragment manager for views

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,10 +1,12 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
 
 
 /*
@@ -23,14 +25,18 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* TODO 1: Load fragment(s)
-            - Show _only_ ButtonFragment if portrait
-            - show _both_ fragments if Landscape
-          */
+        if (findViewById<View>(R.id.container2) == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container1, ButtonFragment())
+                .commit()
+        } else {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container1, ButtonFragment())
+                .replace(R.id.container2, DieFragment())
+        }
     }
-
-    /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
-        */
 
     // This callback function gets invoked when the child Fragment invokes it
     // Remember to place Fragment transactions on BackStack so then can be reversed
